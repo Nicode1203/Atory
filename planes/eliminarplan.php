c168cf474745b1abbc6584bb1c51b41bef5b307e
Nicode1203 <nicolasperezb1203@gmail.com>
2024-02-10 16:10:32 -0500

    Arreglado eliminar planes

--- planes/eliminarplan.php
+++ planes/eliminarplan.php
@@ -2,12 +2,11 @@
 
 include("conexion.php");
 
-$cp=$_GET['cp'];
+$cp = $_GET['cp'];
 
-$sql="UPDATE SET estadoPlan='Archivado' FROM plan WHERE codigoPlan='$cp'";  
-$query=mysqli_query($con,$sql);
+$sql = "UPDATE plan SET estadoPlan='Archivado' WHERE codigoPlan='$cp'";
+$query = mysqli_query($con, $sql);
 
-    if($query){
-        Header("Location: ../planes/tablaplanes.php");
-    }
-?>
+if ($query) {
+    Header("Location: ../planes/tablaplanes.php");
+}
--- tablas.php
+++ tablas.php
@@ -58,7 +58,10 @@ if ($seguridad->getUsuario()==null) {
                     <?php
                     include("conexion.php");
 
-                    $sql = "SELECT * FROM cliente WHERE estadoCliente='Activo';";
+                    $sql = "SELECT cliente.tipoDocumento,cliente.documentoCliente,cliente.nombreCliente,plan.nombrePlan FROM cliente 
+                    INNER JOIN plan
+                    ON cliente.plan_idPlan=plan.idPlan
+                    WHERE estadoCliente='Activo';";
 
                     echo
                     '<div class="table-responsive">
@@ -69,12 +72,7 @@ if ($seguridad->getUsuario()==null) {
               <th> Tipo identificacion </th>
               <th> Numero de documento</th>
               <th> Nombres</th>
-              <th> Telefono</th>
-              <th> email</th>
-              <th> Dirección</th>
-              <th> Estado</th>
-              <th> Fecha de creacion</th>
-              <th> Ultima Actualización</th>
+              <th> Plan</th>
               <th> Actualizar</th>
               <th> Eliminar</th>
             </tr>
@@ -85,23 +83,13 @@ if ($seguridad->getUsuario()==null) {
                             $td = $row['tipoDocumento'];
                             $id = $row['documentoCliente'];
                             $nombres = $row['nombreCliente'];
-                            $telefono = $row['telefonoCliente'];
-                            $email = $row['correoCliente'];
-                            $dir = $row['direccion'];
-                            $estado = $row['estadoCliente'];
-                            $creacion = $row['creado'];
-                            $act = $row['ultimaActualizacion'];
+                            $plan = $row['nombrePlan'];
                     ?>
                             <tr>
                                 <td> <?php echo $td ?></td>
                                 <td> <?php echo $id ?></td>
                                 <td> <?php echo $nombres ?></td>
-                                <td> <?php echo $telefono ?></td>
-                                <td> <?php echo $email ?></td>
-                                <td> <?php echo $dir ?></td>
-                                <td> <?php echo $estado ?></td>
-                                <td> <?php echo $creacion ?></td>
-                                <td> <?php echo $act ?></td>
+                                <td> <?php echo $plan ?></td>
                                 <td>
                                     <a href="actualizar.php?id=<?php echo $row['documentoCliente'] ?>" class="btn btn-info">Editar</a>
                                 </td>
