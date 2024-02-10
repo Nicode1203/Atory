be8614727cf7c57ed32b9b399d2b52cff02189e9
karl schick <schickperez@gmail.com>
2024-02-09 23:41:03 -0500

    footer terminar

--- userTecnico/visitas/tablaConsultada.php
+++ userTecnico/visitas/tablaConsultada.php
@@ -97,12 +97,7 @@ if ($varsesion == null || $varsesion = '') {
 
                 <!-- partial -->
             </div>
-            <footer class="footer">
-                <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Atory Solution 2023</span>
-                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> <a href=" " target="_blank"></a> </span>
-                </div>
-            </footer>
+
             <!-- main-panel ends -->
         </div>
         <!-- page-body-wrapper ends -->
