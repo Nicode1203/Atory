ef47380bdff9488c46ab4cf4f98cf089db789055
fabianquimm <quimf05@gmail.com>
2024-02-13 00:08:38 -0500

    CAMBIO DE NOMBRE BOTON

--- menu/menuint.php
+++ menu/menuint.php
@@ -111,7 +111,7 @@ if ($varsesion == null || $varsesion = '') {
                         <span class="menu-icon">
                             <i class="mdi mdi-contacts"></i>
                         </span>
-                        <span class="menu-title">PQR</span>
+                        <span class="menu-title">Sugerencia Clientes</span>
                     </a>
                 </li>
                 <li class="nav-item menu-items">
