201f2b5a3f46865e6691d1f24256742140136a47
karl schick <schickperez@gmail.com>
2024-02-15 00:45:03 -0500

    fact

--- facturacion/verfactura.php
+++ facturacion/verfactura.php
@@ -21,80 +21,87 @@
 </head>
 
 <body>
-      <!-- partial -->
-      <?php
-      include_once "conexion.php";
+  <!-- partial -->
+  <?php
+  include_once "conexion.php";
 
-      $id=$_GET['id'];       
-      $sql= "SELECT * FROM cliente  
+  $id = $_GET['id'];
+  $sql = "SELECT * FROM cliente  
       INNER JOIN factura
       ON cliente.idCliente=factura.cliente_idCliente
       WHERE idCliente= '$id';";
 
-      if ($rta = $con->query($sql)) {
-        while ($row = $rta->fetch_assoc()) {
-          $id = $row['idCliente'];
-          $td=$row['tipoDocumento'];
-          $doc=$row['documentoCliente'];
-          $nomc=$row['nombreCliente'];
-          $telc=$row['telefonoCliente'];
-          $emailc=$row['correoCliente'];
-          $dc=$row['direccion'];
-          $ec=$row['estadoCliente'];
-          $creado=$row['creado'];
-          $uact=$row['ultimaActualizacion'];
-          $if=$row['idFactura'];
-          $ffact=$row['fechaFactura'];
-          $impt=$row['impuestoTotal'];
-          $st=$row['valorTotalFactura'];
-          $cid=$row['cliente_idCliente'];
-          $estf=$row['estadoFactura'];
-        }
-      }
-      ?>
-      <div class="main-panel">
-      <div class="content-wrapper">
-            <div class="page-header">
-                <h1 style="font-size: 32px;">SU FACTURA</h1>
-                <nav aria-label="breadcrumb">
-                </nav>
-            </div>
-          <div class="row">
-            <div class="col-md-6 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  
-                  <form class="forms-sample">
-                    <div class="form-group"> 
-                    <div class="card-body">
-                    <h4 class="card-title">cliente <?php echo "$nomc" ?></h4>
-                    <h4 class="card-title">Con <?php echo "$td: $doc"?></h4>
-                    <h4 class="card-title">Esta es su factura correspontiente al       :<?php echo "     $ffact"?> </h4> 
-                  
-                  <form class="forms-sample">
-                  <div class="form-group">
-                      <label for="cp"> La cual se encuentra:  <?php echo "$estf"?> </label>
-                    </div>
-                    <div class="form-group">
-                      <label for="cp">Sub total: <?php echo "$st" ?></label>
-                    </div>
-                    <div class="form-group">
-                      <label for="vel">Impuesto: <?php echo "$impt" ?></label>
-                    </div>
-                    <div class="form-group">
-                      <label for="plan">Valor total a pagar: <?php echo " $st" ?></label>
-                    </div>
-                    <div class="form-group">
-                      <label for="des"> Le recordamos su información de contacto, su telefono es <?php echo "$telc" ?> y su correo es: <?php echo "$emailc" ?></label>
-                    </div>
-                    <div class="form-group">
-                      <label for="des"> Tambien le estará llegando una copia a su dirección <?php echo "$dc" ?></label>
-                    </div>
-                    <a href="../index.html" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Volver a pagina inicial</a>
-                  </form>
+  if ($rta = $con->query($sql)) {
+    while ($row = $rta->fetch_assoc()) {
+      $id = $row['idCliente'];
+      $td = $row['tipoDocumento'];
+      $doc = $row['documentoCliente'];
+      $nomc = $row['nombreCliente'];
+      $telc = $row['telefonoCliente'];
+      $emailc = $row['correoCliente'];
+      $dc = $row['direccion'];
+      $ec = $row['estadoCliente'];
+      $creado = $row['creado'];
+      $uact = $row['ultimaActualizacion'];
+      $if = $row['idFactura'];
+      $ffact = $row['fechaFactura'];
+      $impt = $row['impuestoTotal'];
+      $st = $row['valorTotalFactura'];
+      $cid = $row['cliente_idCliente'];
+      $estf = $row['estadoFactura'];
+    }
+  }
+  ?>
+  <div class="main-panel">
+    <div class="content-wrapper">
+      <div class="page-header">
+        <h1 style="font-size: 32px;">SU FACTURA</h1>
+        <nav aria-label="breadcrumb">
+        </nav>
+      </div>
+      <div class="row">
+        <div class="col-md-6 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+
+              <form class="forms-sample">
+                <div class="form-group">
+                  <div class="card-body">
+                    <h4 class="card-title">Hola <?php echo "$nomc" ?></h4>
+                    <h4 class="card-title">Con <?php echo "$td: $doc" ?></h4>
+                    <h4 class="card-title">Pago oportuno :<?php echo "     $ffact" ?> </h4>
+
+
+                      <div class="form-group">
+                        <label for="des"> Su telefono es <?php echo "$telc" ?></label>
+                      </div>
+                      <div class="form-group">
+                         <label for="des"> Su correo es: <?php echo "$emailc" ?></label>
+                      </div>
+                      <div class="form-group">
+                        <label for="des">Su dirección <?php echo "$dc" ?></label>
+                      </div>
+                      <div class="form-group">
+                        <label for="cp"> La cual se encuentra: <?php echo "$estf" ?> </label>
+                      </div>
+                      <div class="form-group">
+                        <h4 label for="cp">Sub total: <?php echo "$st" ?></label></h4>
+                      </div>
+                      <div class="form-group">
+                        <h4 label for="vel">Impuesto: <?php echo "$impt" ?></label></h4>
+                      </div>
+                      <div class="form-group">
+                      <h4 label for="plan">________________________</label></h4>                        
+                        <h4 label for="plan">Valor total a pagar: <?php echo " $st" ?></label></h4>
+                      </div>
+
+                      <a href="../index.html" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Volver a pagina inicial</a>
+
+                  </div>
                 </div>
-              </div>
             </div>
+
+          </div>
 
         </div>
 
@@ -102,15 +109,13 @@
 
     </div>
 
-  </div>
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
 
-  <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-
-  <script src="../assets/js/off-canvas.js"></script>
-  <script src="../assets/js/hoverable-collapse.js"></script>
-  <script src="../assets/js/misc.js"></script>
-  <script src="../assets/js/settings.js"></script>
-  <script src="../assets/js/todolist.js"></script>
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/hoverable-collapse.js"></script>
+    <script src="../assets/js/misc.js"></script>
+    <script src="../assets/js/settings.js"></script>
+    <script src="../assets/js/todolist.js"></script>
 
 </body>
 
