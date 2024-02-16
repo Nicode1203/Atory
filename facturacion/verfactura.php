50ddd91bcb25e6209b5e9f9a84956eb903d9f602
karl schick <schickperez@gmail.com>
2024-02-16 06:22:06 -0500

    factura cambio

--- facturacion/verfactura.php
+++ facturacion/verfactura.php
@@ -2,26 +2,22 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>AtorySolution</title>
-  <!-- plugins:css -->
   <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <!-- End Plugin css for this page -->
-  <!-- inject:css -->
-  <!-- endinject -->
-  <!-- Layout styles -->
   <link rel="stylesheet" href="../assets/css/style.css">
-  <!-- End layout styles -->
   <link rel="shortcut icon" href="../assets/images/favicon.png" />
+  <style>
+    .form-group>div {
+      margin-bottom: 1px;
+      /* Ajusta este valor según lo necesites */
+    }
+  </style>
 </head>
 
 <body>
-  <!-- partial -->
   <?php
   include_once "conexion.php";
 
@@ -33,34 +29,34 @@
   ON cliente.idCliente=factura.cliente_idCliente
   WHERE idCliente= '$id';";
 
-if ($rta = $con->query($sql)) {
-  while ($row = $rta->fetch_assoc()) {
-    $id = $row['idCliente'];
-    $td = $row['tipoDocumento'];
-    $doc = $row['documentoCliente'];
-    $nomc = $row['nombreCliente'];
-    $telc = $row['telefonoCliente'];
-    $emailc = $row['correoCliente'];
-    $dc = $row['direccion'];
-    $ec = $row['estadoCliente'];
-    $plancliente = $row['plan_idPlan'];
-    $creado = $row['creado'];
-    $uact = $row['ultimaActualizacion'];
-    $idplan = $row['idPlan'];
-    $codigoplan = $row['codigoPlan'];
-    $tipoplan = $row['tipoPlan'];
-    $vp = $row['velocidad'];
-    $nombreplan = $row['nombrePlan'];
-    $precioplan = $row['precioPlan'];
-    $descripcionplan = $row['desPlan'];
-    $estadoplan = $row['estadoPlan'];
-    $if = $row['idFactura'];
-    $ffact = $row['fechaFactura'];
-    $impt = $row['impuestoTotal'];
-    $sub=$row['subTotal'];
-    $st = $row['valorTotalFactura'];
-    $cid = $row['cliente_idCliente'];
-    $estf = $row['estadoFactura'];
+  if ($rta = $con->query($sql)) {
+    while ($row = $rta->fetch_assoc()) {
+      $id = $row['idCliente'];
+      $td = $row['tipoDocumento'];
+      $doc = $row['documentoCliente'];
+      $nomc = $row['nombreCliente'];
+      $telc = $row['telefonoCliente'];
+      $emailc = $row['correoCliente'];
+      $dc = $row['direccion'];
+      $ec = $row['estadoCliente'];
+      $plancliente = $row['plan_idPlan'];
+      $creado = $row['creado'];
+      $uact = $row['ultimaActualizacion'];
+      $idplan = $row['idPlan'];
+      $codigoplan = $row['codigoPlan'];
+      $tipoplan = $row['tipoPlan'];
+      $vp = $row['velocidad'];
+      $nombreplan = $row['nombrePlan'];
+      $precioplan = $row['precioPlan'];
+      $descripcionplan = $row['desPlan'];
+      $estadoplan = $row['estadoPlan'];
+      $if = $row['idFactura'];
+      $ffact = $row['fechaFactura'];
+      $impt = $row['impuestoTotal'];
+      $sub = $row['subTotal'];
+      $st = $row['valorTotalFactura'];
+      $cid = $row['cliente_idCliente'];
+      $estf = $row['estadoFactura'];
     }
   }
   ?>
@@ -73,62 +69,83 @@ if ($rta = $con->query($sql)) {
         <div class="col-md-6 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
-              <h4 class="card-title">Estimado cliente  <?php echo "$nomc" ?>, identificado con : <?php echo "$td: $doc" ?></h4>
+              <div style="text-align: center;">
+                <img class="logo" src="../empresa/logoEmpresa.png" alt="logo" style="max-width: 20%; height: auto" class="img-responsive" />
+              </div>
+              <br>
+              <center>
+                <h2 class="card-title">BITS PLAY SAS</h2>
+              </center>
+
+
               <form class="forms-sample">
+
                 <div class="form-group">
                   <div class="card-body">
-                    <h4 class="card-title">Esta factura corresponde a : <?php echo "$ffact" ?> </h4>
+
                     <form class="forms-sample">
+                      <center><class="card-title">PROVEEDORES DE SERVICIO DE INTERNET</center>
+                      <center><class="card-title">NIT. 989988998-8 </center>
+                      <center><class="card-title">tel. +57 312 2800098 </center>
+                      <center><class="card-title">whatsapp +57 318 6005008 </center>
+                      <br>
+                      <h4 class="card-title">Hola <?php echo "$nomc" ?></h4>
+
                       <div class="form-group">
-                        <label for="cp">Corresponde al plan  <?php echo "$nombreplan" ?></label>
+                        <div>
+                          <label for="cp">Con : <?php echo "$td: $doc" ?></label>
+                        </div>
+                        <div>
+                          <label for="des"> Su telefono es <?php echo "$telc" ?></label>
+                        </div>
+                        <div>
+                          <label for="des"> Su correo es: <?php echo "$emailc" ?></label>
+                        </div>
+                        <div>
+                          <label for="cp">Tu factura correspondiente al : <?php echo "$ffact" ?></label>
+                        </div>
+                        <div>
+                          <label for="cp">Con <?php echo "$nombreplan" ?></label>
+                        </div>
+                        <div>
+                          <label for="cp">Tipo: <?php echo "$tipoplan" ?></label>
+                        </div>
+                        <div>
+                          <label for="cp">Velocidad: <?php echo "$vp" ?></label>
+                        </div>
+                        <div>
+                          <label for="cp">Tu factura se encuentra actualmente: <?php echo "$estf" ?> </label>
+                        </div>
+                        <br>
+                        <div>
+                          <center><label for="cp">Sub total: <?php echo "$sub" ?></label></center>
+                        </div>
+                        <div>
+                          <center><label for="vel">IVA 19%: <?php echo "$impt" ?></label></center>
+                        </div>
+                        <div>
+                          <center><label>_______________________</label></center>
+                        </div>
+                        <div>
+                          <center><label for="plan">Valor total a pagar: <?php echo " $st" ?></label></center>
+                        </div>
+
                       </div>
-                      <div class="form-group">
-                        <label for="cp">del tipo: <?php echo "$tipoplan" ?></label>
-                      </div>
-                      <div class="form-group">
-                        <label for="cp">Velocidad: <?php echo "$vp" ?></label>
-                      </div>
-                      <div class="form-group">
-                        <label for="cp">Se encuentra actualmente: <?php echo "$estf" ?> </label>
-                      </div>
-                      <div class="form-group">
-                        <label for="cp">Sub total: <?php echo "$sub" ?></label>
-                      </div>
-                      <div class="form-group">
-                        <label for="vel">Impuesto: <?php echo "$impt" ?></label>
-                      </div>
-                      <div class="form-group">
-                        <label for="plan">Valor total a pagar: <?php echo " $st" ?></label>
-                      </div>
-                      <div class="form-group">
-                        <label for="des"> Le recordamos su información de contacto, su telefono es <?php echo "$telc" ?> y su correo es: <?php echo "$emailc" ?></label>
-                      </div>
+
                       <a href="../index.html" class="btn btn-light btn-lg active" role="button" aria-pressed="true">Volver </a>
                     </form>
                   </div>
                 </div>
             </div>
-
           </div>
-          <!-- partial -->
         </div>
-        <!-- main-panel ends -->
       </div>
-      <!-- page-body-wrapper ends -->
-    </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="../assets/js/off-canvas.js"></script>
-    <script src="../assets/js/hoverable-collapse.js"></script>
-    <script src="../assets/js/misc.js"></script>
-    <script src="../assets/js/settings.js"></script>
-    <script src="../assets/js/todolist.js"></script>
-    <!-- endinject -->
+      <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+      <script src="../assets/js/off-canvas.js"></script>
+      <script src="../assets/js/hoverable-collapse.js"></script>
+      <script src="../assets/js/misc.js"></script>
+      <script src="../assets/js/settings.js"></script>
+      <script src="../assets/js/todolist.js"></script>
 </body>
 
 </html>
